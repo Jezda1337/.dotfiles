59aa9d7a075efd4df1bee99c443b9228fb637dbb
Radoje <megasrbin1@gmail.com>
2024-02-25 00:06:08 +0100

    ..

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,7 +1,5 @@
 return {
   "stevearc/oil.nvim",
-  opts = {},
-  -- Optional dependencies
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
