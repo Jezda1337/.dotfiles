4ff6c27989a426e1902a8d8930f46efec691d8d7
Radoje <megasrbin1@gmail.com>
2024-02-24 23:51:18 +0100

    add oil plugin

---
+++ .config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,10 @@
+return {
+  "stevearc/oil.nvim",
+  opts = {},
+  -- Optional dependencies
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+    require("oil").setup()
+  end,
+}
