4eca3a4911e5fbeadb11d85030ab4550fee53a6c
Radoje <megasrbin1@gmail.com>
2024-06-19 23:29:26 +0200

    update neovim and hyprland
    add the best theme in the game
    set floating window for dev tools firefox/brave/chrome etc..

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,10 +1,19 @@
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
-	opts = {
-		variant = "main",
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		opts = {
+			variant = "main",
+		},
+		config = function()
+			-- vim.cmd("colorscheme rose-pine-main")
+		end,
 	},
-	config = function()
-		vim.cmd("colorscheme rose-pine-main")
-	end,
+	{
+		"blazkowolf/gruber-darker.nvim",
+		name = "best-theme-ever",
+		config = function()
+			vim.cmd("colorscheme gruber-darker")
+		end,
+	},
 }
