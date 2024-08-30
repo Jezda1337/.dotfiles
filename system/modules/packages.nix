1f0ef311685fe7458a8c57641cbfc47c70d5dc13
Radoje <megasrbin1@gmail.com>
2024-08-30 21:25:36 +0200

    create separate packages

---
+++ system/modules/packages.nix
@@ -0,0 +1,15 @@
+{ pkgs, ... }:
+{
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    curl
+    git
+    firefox
+    kitty
+    lazigit
+    gh
+  ]
+}
