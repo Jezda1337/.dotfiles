bcfdffc7bcc9b8d6954a9647d8b6c1d833855eff
Radoje <megasrbin1@gmail.com>
2024-08-30 21:25:39 +0200

    fix type

--- system/modules/networking.nix
+++ system/modules/networking.nix
@@ -2,7 +2,7 @@
 {
   # networking.hostName = "nixos"; Define your hostname.
   # networking.wireless.enable = true; Enable wireless support via wpa_supplicant.
-  networking.networkingmanager.enable = true;
+  networking.networkmanager.enable = true;
 
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
