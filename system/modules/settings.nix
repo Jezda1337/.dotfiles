765fd014c20611dfeb0eb27dc95260bb046dbcfc
Radoje <megasrbin1@gmail.com>
2024-08-31 16:03:09 +0200

    add cache for yazi build

--- system/modules/settings.nix
+++ system/modules/settings.nix
@@ -1,6 +1,6 @@
 {
   nix.settings = {
-    substituters = ["https://hyprland.cachix.org"];
-    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+    substituters = ["https://hyprland.cachix.org" "https://yazi.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k="];
   };
 }
