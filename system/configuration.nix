dffabefdc372f8b45e46237cd55c44d926930862
Radoje <megasrbin1@gmail.com>
2024-09-01 00:18:37 +0200

    add vim as default editor for yazi

--- system/configuration.nix
+++ system/configuration.nix
@@ -99,6 +99,7 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
   ];
+  environment.variables.EDITOR = "vim";
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
