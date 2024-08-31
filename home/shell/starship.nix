9ca1eb1f2facb6460782467a13e5bc730a02cc60
Radoje <megasrbin1@gmail.com>
2024-08-31 14:37:53 +0200

    add starship

--- home/shell/starship.nix
+++ home/shell/starship.nix
@@ -8,6 +8,12 @@
       aws.disabled = true;
       gcloud.disabled = true;
       line_break.disabled = true;
+      character = {
+        success_symbol = "[➜](bold green)";
+        error_symbol = "[➜](bold red)";
+      };
+
+      package.disabled = true;
     };
   };
 }
