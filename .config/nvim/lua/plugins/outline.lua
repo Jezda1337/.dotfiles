d3ed2c2e29c48175a4a7fa53e3db33458097850b
Radoje <megasrbin1@gmail.com>
2024-09-24 01:16:00 +0200

    add outline plugin

---
+++ .config/nvim/lua/plugins/outline.lua
@@ -0,0 +1,15 @@
+return {
+	"hedyhli/outline.nvim",
+	lazy = true,
+	cmd = { "Outline", "OutlineOpen" },
+	keys = {
+		{ "<leader>o", "<cmd>Outline<CR>", desc = "Toggle outline" },
+	},
+	opts = {
+		symbols = {
+			filter = {
+				vue = { "Method" },
+			},
+		},
+	},
+}
