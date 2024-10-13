d0904a5deba626d7fd32c04ea0cbcde9f4f227df
Radoje <megasrbin1@gmail.com>
2024-10-13 18:09:52 +0200

    set oil to ignore *_templ.go files

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -3,6 +3,13 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-		require("oil").setup()
+		require("oil").setup({
+			view_options = {
+				show_hidden = false,
+				is_hidden_file = function(name, _)
+					return vim.startswith(name, ".") or vim.endswith(name, "_templ.go")
+				end,
+			},
+		})
 	end,
 }
