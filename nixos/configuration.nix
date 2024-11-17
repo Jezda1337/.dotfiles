4cda1913c5d2e293960b95d6be7a552f27e040e0
Radoje <megasrbin1@gmail.com>
2024-11-18 00:16:13 +0100

    update

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -12,6 +12,23 @@
       ./modules
     ];
 
+	security.polkit.enable = true;
+	systemd = {
+		user.services.polkit-gnome-authentication-agent-1 = {
+			description = "polkit-gnome-authentication-agent-1";
+			wantedBy = [ "graphical-session.target" ];
+			wants = [ "graphical-session.target" ];
+			after = [ "graphical-session.target" ];
+			serviceConfig = {
+				Type = "simple";
+				ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+				Restart = "on-failure";
+				RestartSec = 1;
+				TimeoutStopSec = 10;
+			};
+		};
+	};
+
 	stylix.enable = true;
 	stylix.cursor.package = pkgs.banana-cursor;
 	stylix.cursor.name = "Banana";
@@ -319,6 +336,7 @@
 			alsa-tools
 			clapper
 			libxkbcommon # keyboard supp lib for wayland and manymore
+			gparted
 
 
       tor-browser
