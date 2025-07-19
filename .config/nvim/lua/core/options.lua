218504d6144da7e8b8d0ceac5c625103cedd3d97
Radoje <megasrbin1@gmail.com>
2025-07-19 23:16:47 +0200

    add netrw as default file explorer

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -1,53 +1,61 @@
+vim.g.netrw_banner = 0
+vim.g.netrw_browse_split = 0
+vim.g.netrw_altv = 1
+vim.g.netrw_liststyle = 1
+vim.g.netrw_sort_by = "extend"
+
 local options = {
-  ex = true,
-  expandtab = true,
-  softtabstop = 2,
-  tabstop = 2,
-  shiftwidth = 2,
-  showmode = false,
-  breakindent = true,
-  undofile = true,
-  ignorecase = true,
-  smartcase = true,
-  termguicolors = true,
-  number = true,
-  relativenumber = true,
-  backup = false,
-  background = "dark",
-  hidden = true,        -- enable background buffers
-  hlsearch = true,
-  incsearch = true,     -- show the match while typing
-  joinspaces = false,   -- no double space with join
-  list = false,
-  listchars = { tab = "  ", trail = "·", nbsp = "⍽", eol = "↴" },
-  fillchars = { eob = " " },   -- remove ~
-  scrolloff = 4,
-  sidescrolloff = 4,
-  signcolumn = "yes",
-  splitbelow = true,
-  splitright = true,
-  inccommand = "split",
-  cursorline = true,
-  title = true,
-  updatetime = 250,
-  timeoutlen = 300,
-  mouse = "a",
-  laststatus = 2,
-  -- statusline = "%t%h%m%r%=%c,%l/%L %P",
-  cmdheight = 0,
-  clipboard = "unnamedplus",
-  wrap = false,
-  autoread = true,
-  spell = true,
-  completeopt = "menuone,fuzzy,noinsert,popup",
-  completeitemalign = "kind,abbr,menu",
-  grepprg = "grep -inH",
-  wildmenu = true,
-  wildmode = "longest:full,full"
+    ex = true,
+    expandtab = true,
+    softtabstop = 2,
+    tabstop = 2,
+    shiftwidth = 2,
+    showmode = false,
+    breakindent = true,
+    undofile = true,
+    ignorecase = true,
+    smartcase = true,
+    termguicolors = true,
+    number = true,
+    relativenumber = true,
+    backup = false,
+    background = "dark",
+    hidden = true,      -- enable background buffers
+    hlsearch = true,
+    incsearch = true,   -- show the match while typing
+    joinspaces = false, -- no double space with join
+    list = false,
+    listchars = { tab = "  ", trail = "·", nbsp = "⍽", eol = "↴" },
+    fillchars = { eob = " " }, -- remove ~
+    scrolloff = 4,
+    sidescrolloff = 4,
+    signcolumn = "yes",
+    splitbelow = true,
+    splitright = true,
+    inccommand = "split",
+    cursorline = true,
+    title = true,
+    updatetime = 250,
+    timeoutlen = 300,
+    mouse = "a",
+    laststatus = 2,
+    -- statusline = "%t%h%m%r%=%c,%l/%L %P",
+    cmdheight = 0,
+    clipboard = "unnamedplus",
+    wrap = false,
+    autoread = true,
+    spell = true,
+    completeopt = "menuone,fuzzy,noinsert,popup",
+    completeitemalign = "kind,abbr,menu",
+    grepprg = "grep -inH",
+    wildmenu = true,
+    wildmode = "longest:full,full",
+    wildoptions = "pum",
+    guifont = "monospace:h17"
 }
 
 for key, value in pairs(options) do
-  vim.opt[key] = value
+    vim.opt[key] = value
 end
 
 vim.opt.path:append("**")
@@ -59,40 +67,40 @@ vim.opt.wildignore:append({ "*/node_modules/*,*/.history/*,*/dist/*,*/.git/*" })
 vim.opt.grepprg = "rg --vimgrep --hidden --glob=!node_modules --glob=!.history --glob=!dist --glob=!.git"
 
 local function fold_virt_text(result, s, lnum, coloff)
-  if not coloff then
-    coloff = 0
-  end
-  local text = ""
-  local hl
-  for i = 1, #s do
-    local char = s:sub(i, i)
-    local hls = vim.treesitter.get_captures_at_pos(0, lnum, coloff + i - 1)
-    local _hl = hls[#hls]
-    if _hl then
-      local new_hl = "@" .. _hl.capture
-      if new_hl ~= hl then
-        table.insert(result, { text, hl })
-        text = ""
-        hl = nil
-      end
-      text = text .. char
-      hl = new_hl
-    else
-      text = text .. char
+    if not coloff then
+        coloff = 0
     end
-  end
-  table.insert(result, { text, hl })
+    local text = ""
+    local hl
+    for i = 1, #s do
+        local char = s:sub(i, i)
+        local hls = vim.treesitter.get_captures_at_pos(0, lnum, coloff + i - 1)
+        local _hl = hls[#hls]
+        if _hl then
+            local new_hl = "@" .. _hl.capture
+            if new_hl ~= hl then
+                table.insert(result, { text, hl })
+                text = ""
+                hl = nil
+            end
+            text = text .. char
+            hl = new_hl
+        else
+            text = text .. char
+        end
+    end
+    table.insert(result, { text, hl })
 end
 
 function _G.custom_foldtext()
-  local start = vim.fn.getline(vim.v.foldstart):gsub("\t", string.rep(" ", vim.o.tabstop))
-  local end_str = vim.fn.getline(vim.v.foldend)
-  local end_ = vim.trim(end_str)
-  local result = {}
-  fold_virt_text(result, start, vim.v.foldstart - 1)
-  table.insert(result, { " ... ", "Delimiter" })
-  fold_virt_text(result, end_, vim.v.foldend - 1, #(end_str:match("^(%s+)") or ""))
-  return result
+    local start = vim.fn.getline(vim.v.foldstart):gsub("\t", string.rep(" ", vim.o.tabstop))
+    local end_str = vim.fn.getline(vim.v.foldend)
+    local end_ = vim.trim(end_str)
+    local result = {}
+    fold_virt_text(result, start, vim.v.foldstart - 1)
+    table.insert(result, { " ... ", "Delimiter" })
+    fold_virt_text(result, end_, vim.v.foldend - 1, #(end_str:match("^(%s+)") or ""))
+    return result
 end
 
 vim.opt.foldtext = "v:lua.custom_foldtext()"
